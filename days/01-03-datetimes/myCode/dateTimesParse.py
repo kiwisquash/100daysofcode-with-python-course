c71713a5a73efdfe3183ac391603a546d8c0d7f3
Ki Song <kiwisquash@gmail.com>
2018-11-25 17:57:19 -0800

    Implemetned functions and added a basic test.

--- days/01-03-datetimes/myCode/dateTimesParse.py
+++ days/01-03-datetimes/myCode/dateTimesParse.py
@@ -15,15 +15,14 @@ from datetime import datetime
 
 THIS_YEAR = 2018
 
-
 def years_ago(date):
     """Receives a date string of 'DD MMM, YYYY', for example: 8 Aug, 2015
        Convert this date str to a datetime object (use strptime).
        Then extract the year from the obtained datetime object and subtract
        it from the THIS_YEAR constant above, returning the int difference.
        So in this example you would get: 2018 - 2015 = 3"""
-    pass
-
+    dateobject = datetime.strptime(date,"%d %b, %Y")
+    return THIS_YEAR - dateobject.year
 
 def convert_eu_to_us_date(date):
     """Receives a date string in European format of dd/mm/yyyy, e.g. 11/03/2002
@@ -31,8 +30,11 @@ def convert_eu_to_us_date(date):
        To enforce the use of datetime's strptime / strftime (over slicing)
        the tests check if a ValueError is raised for invalid day/month/year
        ranges (no need to code this, datetime does this out of the box)"""
-    pass
+    dateobject = datetime.strptime(date,"%d/%m/%Y")
+    return dateobject.strftime("%m/%d/%Y") 
 
+print(years_ago('8 Aug, 2015'))
+print(convert_eu_to_us_date('11/03/2002'))
 # import pytest
 
 # from dt_convert import years_ago, convert_eu_to_us_date
