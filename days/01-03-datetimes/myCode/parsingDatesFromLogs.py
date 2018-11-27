fa419f525c52c37d552396a3a59f8c08d17d5683
Ki Song <kiwisquash@gmail.com>
2018-11-26 22:57:59 -0800

    Successfully implemented time_between_shutdowns in parsingDatesFromLogs.py

--- days/01-03-datetimes/myCode/parsingDatesFromLogs.py
+++ days/01-03-datetimes/myCode/parsingDatesFromLogs.py
@@ -28,19 +28,39 @@ def convert_to_datetime(line):
        returns:
        datetime(2014, 7, 3, 23, 27, 51)'''
     blankIndex1 = line.find(" ")
-    blankIndex2 = line[blankIndex1 + 1:].find(" ") + blankIndex1 + 1
-    print(blankIndex1, blankIndex2)
-    timeString = line[blankIndex1 + 1:blankIndex2]
+    if blankIndex1 !=-1:
+        blankIndex2 = line[blankIndex1 + 1:].find(" ") + blankIndex1 + 1
+        timeString = line[blankIndex1 + 1:blankIndex2]
+        return datetime.strptime(timeString,"%Y-%m-%dT%H:%M:%S")
     # print(timeString) 
 
 # Quick test to see if the times are being extracted correctly
-for line in loglines:
-    print(line)
-    convert_to_datetime(line)
+# for line in loglines:
+#     print(convert_to_datetime(line))
+
+
+def first_shutdown(loglines):
+    for line in loglines:
+        if "Shutdown initiated" in line:
+            return line
+
+def last_shutdown(loglines):
+    for line in loglines[::-1]:
+        if "Shutdown initiated" in line:
+            return line
+
+# Quick test to see if the first_shutdown and last_shutdown are working
+# print(first_shutdown(loglines))
+# print(last_shutdown(loglines))
 
 def time_between_shutdowns(loglines):
     '''TODO 2:
        Extract shutdown events ("Shutdown initiated") from loglines and calculate the 
        timedelta between the first and last one. 
        Return this datetime.timedelta object.'''
-    pass
+    first = convert_to_datetime(first_shutdown(loglines))
+    last = convert_to_datetime(last_shutdown(loglines))
+    return last - first
+
+print(time_between_shutdowns(loglines))
+print(type(time_between_shutdowns(loglines)))
