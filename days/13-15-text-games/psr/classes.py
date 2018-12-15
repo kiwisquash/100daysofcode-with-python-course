39d7452c3de68c1d027239f03fcb0a45b8ecf395
Ki Song <kiwisquash@gmail.com>
2018-12-15 14:21:33 -0800

    Updated classes to condense some information. Removed the compareHand method

--- days/13-15-text-games/psr/classes.py
+++ days/13-15-text-games/psr/classes.py
@@ -1,26 +1,24 @@
+from collections import defaultdict
+
+winData = {
+    "paper" : ["rock"],
+    "rock" : ["scissors"],
+    "scissors" : ["paper"],
+}
+
+loseData = {
+    "scissors" : ["rock"],
+    "paper" : ["scissors"],
+    "rock" : ["paper"],
+}
+
+
 class Player():
     def __init__(self, name):
         self.name = name
 
-class Hand():
-    def __init__(self, handName):
-        self.handName = handName
-    def handCompare(self, hand):
-        if self.handName == "rock" and hand.handName == "rock":
-            return "Draw"
-        if self.handName == "rock" and hand.handName == "paper":
-            return "Lose"
-        if self.handName == "rock" and hand.handName == "scissors":
-            return "Win"
-        if self.handName == "paper" and hand.handName == "rock":
-            return "Win"
-        if self.handName == "paper" and hand.handName == "paper":
-            return "Draw"
-        if self.handName == "paper" and hand.handName == "scissors":
-            return "Lose"
-        if self.handName == "scissors" and hand.handName == "rock":
-            return "Lose"
-        if self.handName == "scissors" and hand.handName == "paper":
-            return "Win"
-        if self.handName == "scissors" and hand.handName == "scissors":
-            return "Draw"
+class Roll():
+    def __init__(self, name):
+        self.name = name
+        self.winsAgainst = winData[name]
+        self.loseAgainst = loseData[name]
