81728dd1818d6161e8079f8e54df8ca3fd692047
Ki Song <kiwisquash@gmail.com>
2018-12-15 21:25:34 -0800

    Simplified the code with the assumption that draw is only possible when two rolls have the same name.

--- days/13-15-text-games/psr/classes.py
+++ days/13-15-text-games/psr/classes.py
@@ -1,22 +1,9 @@
-from collections import defaultdict
-
 # This is the most basic version
 winData = {
     "paper" : ["rock"],
     "rock" : ["scissors"],
     "scissors" : ["paper"],
 }
-
-winData = defaultdict(list,winData)
-loseData = defaultdict(list)
-
-for key1 in winData.keys():
-    for key2 in winData.keys():
-        if key1 == key2:
-            pass
-        else:
-            if key1 in winData[key2]:
-                loseData[key1].append(key2)
 
 class Player():
     def __init__(self, name):
@@ -27,8 +14,7 @@ class Roll():
     def __init__(self, name):
         self.name = name
         self.winAgainst = winData[name]
-        self.loseAgainst = loseData[name]
     def winsTo(self, roll):
         return roll.name in self.winAgainst
     def losesTo(self, roll):
-        return roll.name in self.loseAgainst
+        return not ((roll.name in self.winAgainst) or self.name == roll.name)
