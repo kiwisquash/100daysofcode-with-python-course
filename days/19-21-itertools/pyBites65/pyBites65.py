b37012105a2abfcc442f20b0e4b0bcefdde6484d
Ki Song <kiwisquash@gmail.com>
2018-12-27 18:04:53 -0800

    Updated _get_permutations_draw to address all the issues stated in the previous commit message.

--- days/19-21-itertools/pyBites65/pyBites65.py
+++ days/19-21-itertools/pyBites65/pyBites65.py
@@ -9,7 +9,6 @@ urllib.request.urlretrieve('http://bit.ly/2iQ3dlZ', DICTIONARY)
 with open(DICTIONARY) as f:
     dictionary = set([word.strip().lower() for word in f.read().split()])
 
-
 def get_possible_dict_words(draw):
     """Get all possible words from a draw (list of letters) which are
        valid dictionary words. Use _get_permutations_draw and provided
@@ -26,4 +25,15 @@ def get_possible_dict_words(draw):
 def _get_permutations_draw(draw):
     """Helper to get all permutations of a draw (list of letters), hint:
        use itertools.permutations (order of letters matters)"""
-    return ["".join(letters) for letters in itertools.permutations(draw)]
+    draw = [letter.lower().strip() for letter in draw]
+    no_repeat_perms = []
+    num_letters = len(draw)
+    for num in range(2,num_letters+1):
+        for letters in itertools.permutations(draw, num):
+            word = "".join(letters)
+            if word not in no_repeat_perms:
+                no_repeat_perms.append(word)
+    return no_repeat_perms
+
+if __name__ == "__main__":
+    print(get_possible_dict_words(["T", "I", "I", "G", "T", "T", "L"]))
